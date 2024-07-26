6d5ef1a2d7c7eae72400d0b83cd3a76bc4019393
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2024-07-26 22:28:03 +0800

    patched init.c to shutdown selinux; new rc file for usb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Replace patched init.c in order to shut down selinux
+rm system/core/init/init.c
+cp device/xiaoxun/sl8521e_1h10ll_sw763/init.c system/core/init/init.c
+
 add_lunch_combo omni_sl8521e_1h10ll_sw763-user
 add_lunch_combo omni_sl8521e_1h10ll_sw763-userdebug
 add_lunch_combo omni_sl8521e_1h10ll_sw763-eng
